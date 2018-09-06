cfd65b1ce68483d2aefd84f82567cbd07cc9a204
Andre Rafael <andre@origamid.com>
2018-09-06 19:53:24 +0100

    adicionada arrow nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-console.log(slide);
+slide.addArrow('.prev', '.next');
 
-slide.changeSlide(3);
-slide.activePrevSlide();
+console.log(slide);
--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export default class Slide {
+export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper);
@@ -127,6 +127,9 @@ export default class Slide {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
 
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+
     this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
@@ -137,5 +140,17 @@ export default class Slide {
     this.slidesConfig();
     this.addResizeEvent();
     return this;
+  }
+}
+
+export class SlideNav extends Slide {
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener('click', this.activePrevSlide);
+    this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 }
