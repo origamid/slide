02dd74f421bd57a3ab4d1727688d89106969e215
Andre Rafael <andre@origamid.com>
2018-09-07 13:05:42 +0100

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-import { SlideNav } from './slide.js';
+import SlideNav from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-
-slide.addControl();
-
-console.log(slide);
+slide.addControl('.custom-controls');
